b27b794eddec82c02d29ee532eca1f9dd1c30138
Maxim <pogchamp1401@gmail.com>
2022-09-05 16:02:40 +0300

    some adaptive

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import './App.scss';
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Registration from "./components/Registration/Registration";
-import {useEffect, useState} from "react";
-import axios from "axios";
+import MyBtn from "./components/UI/Button/MyBtn";
 
 function App() {
-
 
 
     return (
         <div className="App">
             <Header/>
-            <img src={require('./media/pexels-alexandr-podvalny-1227513.jpeg')} alt='some' className={"myImg"}/>
+            <div className={"imgContainer"}>
+                <div className="textOnImage">
+                    <h1>Test assignment for front-end developer</h1>
+                    <a>What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a
+                        vast understanding of User design thinking as they'll be building web interfaces with accessibility
+                        in mind. They should also be excited to learn, as the world of Front-End Development keeps
+                        evolving.</a>
+                    <div></div>
+                    <MyBtn text='Sign Up' active={false}></MyBtn>
+                </div>
+            </div>
             <List/>
             <Registration/>
         </div>
